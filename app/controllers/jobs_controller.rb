872df826ffb814a38d0e23fffe0bf76f001d9c8e
abrian rodriguez <pazuzublue111@gmail.com>
2017-12-11 21:56:59 -0500

    added C functionality - >index,new,show,private methods

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,16 +1,50 @@
 class JobsController < ApplicationController
-  def pages
-  end
+  #before anything call the finjob method to find a job fro show, edit, update and destroy
+  before_action :findJob, only: [:show, :edit, :update, :destroy]
 
   def index
+    @jobs = Job.all
+  end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.new(jobParams)
+
+    if @job.save
+      #add to db and redirect to that new job
+      redirect_to @job
+    else
+      #render new page
+      :new
+    end
+  end
+
+  def update
+
+
+  end
+
+  def destroy
+
   end
 
   def show
   end
 
-  def new
-  end
-
   def edit
   end
+
+private
+  def jobParams
+    params.require(:job).permit(:title,:company,:about,:url)
+
+  end
+
+  def findJob
+    @job = Job.find(params[:id])
+  end
+
 end
