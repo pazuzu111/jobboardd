6c3155cc5260e3031f7501ecffb5f8c005718d93
abrian rodriguez <pazuzublue111@gmail.com>
2017-12-11 21:17:34 -0500

    deleted pages view, added resources :jobs to routes, added a root

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
-  get 'jobs/pages'
 
-  get 'jobs/index'
+  #append all controller methods to jobs route - example-> get 'jobs/index'
+  resources :jobs
 
-  get 'jobs/show'
-
-  get 'jobs/new'
-
-  get 'jobs/edit'
+  root 'jobs#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
