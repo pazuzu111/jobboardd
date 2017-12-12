e0c9c8b5c4cc63d88a73106c97aed1941f4d3d6e
abrian rodriguez <pazuzublue111@gmail.com>
2017-12-11 21:11:04 -0500

    rails g controller jobs pages index show new edit - created jobs controller with a pages directory inside of views with 4 erb views

---
+++ app/controllers/jobs_controller.rb
@@ -0,0 +1,16 @@
+class JobsController < ApplicationController
+  def pages
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'jobs/pages'
+
+  get 'jobs/index'
+
+  get 'jobs/show'
+
+  get 'jobs/new'
+
+  get 'jobs/edit'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
